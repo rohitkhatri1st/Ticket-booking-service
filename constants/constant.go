739e09b8b99646cb2f9cbf2c9671bcd41f2804b3
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-08-31 15:23:17 +0530

    Update constant.go
    Adding a constant corresponding to a Houseful.

--- constants/constant.go
+++ constants/constant.go
@@ -7,4 +7,5 @@ const (
 	MARSHAL_ERROR       = "Error occurred while marshalling this struct"
 	SEAT_ALREADY_BOOKED = "This seat has already been booked"
 	NO_USER_FOUND_      = "No Such User Exists."
+	MAX_TICKET_CAPACITY_REACHED = "Theatre is housefull."
 )
